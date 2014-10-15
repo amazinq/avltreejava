111b50e403d279fd534e03bf077bf92904d1a6e1
dqi12.wissmann@gmail.com <dqi12.wissmann@gmail.com@c5983ce2-0157-c935-c1a1-0bcc6e0ae741>
2014-10-15 17:37:00 +0000

    Tree test

--- trunk/AVL_Baum/src_test/de/szut/baum/test/Tree_Test.java
+++ trunk/AVL_Baum/src_test/de/szut/baum/test/Tree_Test.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import de.szut.baum.ComparableObject;
 import de.szut.baum.Tree;
 
 public class Tree_Test {
@@ -16,9 +17,10 @@ public class Tree_Test {
 		
 		int[] unsortedList = {4,3,6,2,1,5,-3,-1,-2};
 		int[] sortedList = {-3,-2,-1,1,2,3,4,5,6};
-		
-		
-		
+		Integer a = 2;
+		for(int i : unsortedList) {
+			tree.addValue(new ComparableObject<Integer>((Integer)i));
+		}
 		
 	}
 
