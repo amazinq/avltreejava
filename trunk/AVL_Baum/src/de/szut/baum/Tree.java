f754740ed3725cb5c135a512bdeede1dcfb752aa
dqi12.wissmann@gmail.com <dqi12.wissmann@gmail.com@c5983ce2-0157-c935-c1a1-0bcc6e0ae741>
2014-10-26 18:36:31 +0000

    Implemented Tests
    Implemented Methods: 
     - AddValue
     - ContainsValue

--- trunk/AVL_Baum/src/de/szut/baum/Tree.java
+++ trunk/AVL_Baum/src/de/szut/baum/Tree.java
@@ -1,42 +1,89 @@
 package de.szut.baum;
 
 public class Tree {
-	
+
 	private Node root;
-	
+
 	public Tree() {
 		root = null;
 	}
 
-	public void addValue(ComparableObject<?> value) {
-		
+	public void addValue(ComparableObject value) {
+
+		boolean valueAdded = false;
+		Node currentNode = root;
+
+		if (!containsValue(value)) {
+			if (root == null) {
+				root = new Node(value, null);
+			}
+			while (!valueAdded) {
+				if (value.getObject().compareTo(
+						(currentNode.getValue().getObject())) == -1) {
+					if (currentNode.getLeftNode() == null) {
+						currentNode.setLeftNode(new Node(value, currentNode));
+						valueAdded = true;
+					} else {
+						currentNode = currentNode.getLeftNode();
+					}
+				} else {
+					if (currentNode.getRightNode() == null) {
+						currentNode.setRightNode(new Node(value, currentNode));
+						valueAdded = true;
+					} else {
+						currentNode = currentNode.getRightNode();
+					}
+				}
+			}
+		}
 	}
-	
+
 	public void deleteValue(ComparableObject<?> value) {
-		
+
 	}
-	
-	public boolean containsValue() {
-		return true;
+
+	public boolean containsValue(ComparableObject value) {
+		Node currentNode = root;
+		boolean valueFound = false;
+		while (!valueFound) {
+			if (currentNode.getValue().getObject().compareTo(value) == 0) {
+				return true;
+			} else {
+				if (value.getObject().compareTo((currentNode.getValue().getObject())) == -1) {
+					if(currentNode.getLeftNode() != null) {
+						currentNode = currentNode.getLeftNode();
+					} else {
+						return false;
+					}
+				} else {
+					if(currentNode.getRightNode() != null) {
+						currentNode = currentNode.getRightNode();
+					} else {
+						return false;
+					}
+				}
+			}
+		}
+		return valueFound;
 	}
-	
+
 	public int getSize() {
 		return 0;
 	}
-	
+
 	public int getHeight() {
 		return 0;
 	}
-	
+
 	public ComparableObject<?> getBiggest() {
 		return null;
 	}
-	
+
 	public ComparableObject<?> getSmallest() {
 		return null;
 	}
-	
-	public void getAllValues() {
-		
+
+	public int[] getAllValues() {
+		return null;
 	}
 }
